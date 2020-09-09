6f083510ca844832431dcd964f4a5a531a7b91af
= <goyal.kumarabhishek@gmail.com>
2020-09-09 18:56:23 +0530

    User-defined Exceptions

---
+++ Errors and Exceptions/User-defined Exceptions.py
@@ -0,0 +1,37 @@
+# User-defined Exceptions:- Programs may name their own exceptions by creating a new exception class 
+# more details about class "https://docs.python.org/3/tutorial/classes.html#tut-classes"
+
+
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class InputError(Error):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
+
+class TransitionError(Error):
+    """Raised when an operation attempts a state transition that's not
+    allowed.
+
+    Attributes:
+        previous -- state at beginning of transition
+        next -- attempted new state
+        message -- explanation of why the specific transition is not allowed
+    """
+
+    def __init__(self, previous, next, message):
+        self.previous = previous
+        self.next = next
+        self.message = message
+
+# Most exceptions are defined with names that end in “Error”, similar to the naming of the standard exceptions
