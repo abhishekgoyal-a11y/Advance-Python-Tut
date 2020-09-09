f57237658e394563b6cec4cf02c5c6437a4b5852
= <goyal.kumarabhishek@gmail.com>
2020-09-09 20:09:50 +0530

    Iterate all keys

---
+++ Iterators for Dictionaries/Iterate all keys.py
@@ -0,0 +1,22 @@
+statesAndCapitals = { 
+					'Gujarat' : 'Gandhinagar', 
+					'Maharashtra' : 'Mumbai', 
+					'Rajasthan' : 'Jaipur', 
+					'Bihar' : 'Patna'
+					} 
+					
+print('List Of given states:\n') 
+
+# Iterating over keys 
+for state in statesAndCapitals: 
+	print(state) 
+
+
+Output:-
+
+List Of given states:
+
+Gujarat
+Maharashtra
+Rajasthan
+Bihar
