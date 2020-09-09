55c66cac5961b24e740231e27431eb76f2e1c82f
= <goyal.kumarabhishek@gmail.com>
2020-09-09 20:10:17 +0530

    Iterate all key value pairs

---
+++ Iterators for Dictionaries/Iterate all key value pairs.py
@@ -0,0 +1,22 @@
+statesAndCapitals = { 
+					'Gujarat' : 'Gandhinagar', 
+					'Maharashtra' : 'Mumbai', 
+					'Rajasthan' : 'Jaipur', 
+					'Bihar' : 'Patna'
+					} 
+					
+print('List Of given states and their capitals:\n') 
+
+# Iterating over values 
+for state, capital in statesAndCapitals.items(): 
+	print(state, ":", capital) 
+
+
+Output:-
+
+List Of given states and their capitals:
+
+Bihar : Patna
+Gujarat : Gandhinagar
+Rajasthan : Jaipur
+Maharashtra : Mumbai
