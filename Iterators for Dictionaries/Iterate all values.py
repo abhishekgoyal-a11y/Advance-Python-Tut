ede5f2ac94e8176f6455926f8c94028ed405029c
= <goyal.kumarabhishek@gmail.com>
2020-09-09 20:09:28 +0530

    Iterate all values

---
+++ Iterators for Dictionaries/Iterate all values.py
@@ -0,0 +1,22 @@
+statesAndCapitals = { 
+					'Gujarat' : 'Gandhinagar', 
+					'Maharashtra' : 'Mumbai', 
+					'Rajasthan' : 'Jaipur', 
+					'Bihar' : 'Patna'
+					} 
+					
+print('List Of given capitals:\n') 
+
+# Iterating over values 
+for capital in statesAndCapitals.values(): 
+	print(capital) 
+
+
+Output:-
+
+List Of given capitals:
+
+Gandhinagar
+Mumbai
+Jaipur
+Patna
