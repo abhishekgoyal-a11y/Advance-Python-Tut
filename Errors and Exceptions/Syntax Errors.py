4c1eefe108a6c686945da6189d757d9fa9fe3a4a
= <goyal.kumarabhishek@gmail.com>
2020-09-09 18:27:33 +0530

    Syntax Errors

---
+++ Errors and Exceptions/Syntax Errors.py
@@ -0,0 +1,36 @@
+# Syntax Errors:- It is also known as parsing errors, are perhaps the most common kind of complaint you get while you are still learning Python
+
+# Example 1
+
+while False print('Hello world')
+
+# Error: Colon(:) is missing before print statement
+
+# Example 2
+
+ages = {
+    'pam': 24,
+    'jim': 24
+    'michael': 43
+}
+
+# Error: Comma(,) is missing after 24
+
+# Example 3
+
+len('hello') = 5
+
+# Error: can't assign to function call
+
+# Example 4
+
+'foo' = 1
+
+# Error: can't assign to literal
+
+# Example 5
+
+1 = 'foo'
+
+# Error: can't assign to literal
+
